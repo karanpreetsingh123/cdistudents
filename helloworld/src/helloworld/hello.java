df2c38bfc55d6155fc4a3a0eaad31a6fcf70e7b1
m1306 <m1306@MANINDER>
2023-12-01 14:08:19 -0500

    new hello world class

---
+++ helloworld/src/helloworld/hello.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class hello {
+public static void main (String[] args)
+{
+System.out.println("karan");
+int a = 5 ; int b = 10 ;
+int c =a+b ;
+System.out.println("sum="+c);
+}
+}
